6ef60edc60e5a5cc9da697d17640f33bcbabcad6
Valerii Lider <valerii.lider@gmail.com>
2021-02-18 01:00:21 +0100

    added Envelope function which wraps given etree.Document into SOAP Envelope

--- gen.go
+++ gen.go
@@ -464,13 +464,36 @@ func Generate(params *Params) error {
 		return nil
 	}
 
+	doc, err = Envelope(doc)
+	if err != nil {
+		return nil
+	}
+	return doc.WriteToFile(params.OutFile)
+}
+
+// Envelope wraps up given RegisterInvoiceRequest into standard SOAP Envelope
+func Envelope(req *etree.Document) (*etree.Document, error) {
+	doc := etree.NewDocument()
+
+	root := doc.CreateElement("s:Envelope")
+	root.CreateAttr("xmlns:s", "http://schemas.xmlsoap.org/soap/envelope/")
+	body := root.CreateElement("s:Body")
+	body.CreateAttr("xmlns:xsd", "http://www.w3.org/2001/XMLSchema")
+	body.CreateAttr("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+	if len(req.Child) != 1 {
+		return nil, fmt.Errorf("Invalid XML document")
+	}
+	body.AddChild(req.Child[0])
+
 	doc.IndentTabs()
 	// removes extra \n at the ned of the docuemnt
 	doc.Root().SetTail("")
 
-	return doc.WriteToFile(params.OutFile)
+	return doc, nil
 }
 
+// helper for reading user input
 func scan(message string) string {
 	fmt.Print(message)
 	var value string
